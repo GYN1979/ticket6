62d9ce54f83b9957e6bed4f5ca53b86f8c6eaa8d
GYN1979 <galeeva.y@yandex.ru>
2022-11-12 20:33:59 +0400

    выполнение задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Необходимо реализовать следующий метод:
+        1. Получаем на входе массив чисел.
+        2. Все четные числа увеличиваем на единицу.
+        3. Возвращаем кусок списка с 3-го по 7-й элемент.
+         */
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        example(arr);
     }
-}
+    public static int[] example(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] += 1;
+            }
+            if (i >= 2 && i < 7) {
+                    System.out.println(arr[i] + "");
+                }
+        }
+        return arr;
+    }
+    }
